9f64a3b35f58f3c33e8e4655ffbeec9b14d6d181
Adam Faidy <faidyadam1@gmail.com>
2021-01-25 17:18:05 -0500

    seed command routes + init__py auth,user,post,comment routes

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,10 @@ from flask_wtf.csrf import CSRFProtect, generate_csrf
 from flask_login import LoginManager
 
 from .models import db, User
-from .api.user_routes import user_routes
-from .api.auth_routes import auth_routes
+from .api import user_routes
+from .api import auth_routes
+from .api import post_routes
+from .api import comment_routes
 
 from .seeds import seed_commands
 
@@ -31,6 +33,8 @@ app.cli.add_command(seed_commands)
 app.config.from_object(Config)
 app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
+app.register_blueprint(post_routes, url_prefix='/api/posts')
+app.register_blueprint(comment_routes, url_prefix='/api/comments')
 db.init_app(app)
 Migrate(app, db)
 
