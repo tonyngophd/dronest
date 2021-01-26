e2f8eef9fac23afdc718226fd84cadf2273fac32
Adam Faidy <faidyadam1@gmail.com>
2021-01-26 16:02:57 -0500

    AWS s3 finalized pre-test

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
 import os
 from flask import Flask, render_template, request, session, redirect
+from werkzeug.utils import secure_filename
 from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect, generate_csrf
 from flask_login import LoginManager
+from .helpers import *
+
+
 
 from .models import * 
 from .api import user_routes
@@ -78,3 +82,45 @@ def react_root(path):
     if path == 'favicon.ico':
         return app.send_static_file('favicon.ico')
     return app.send_static_file('index.html')
+
+
+
+# move to respective route
+
+@app.route("/s3_upload", methods=["POST"])
+def upload_file():
+
+	# A
+    if "user_file" not in request.files:
+        return "No user_file key in request.files"
+
+	# B
+    file    = request.files["user_file"]
+
+    """
+        These attributes are also available
+
+        file.filename    # The actual name of the file
+        file.content_type
+        file.content_length
+        file.mimetype
+
+    """
+
+
+    
+
+	# C.
+    if file.filename == "":
+        return "Please select a file"
+
+	# D.
+    if file and allowed_file(file.filename):
+        file.filename = secure_filename(file.filename)
+        output   	  = upload_file_to_s3(file, app.config["S3_BUCKET"])
+        return str(output)
+
+    else:
+        return redirect("/")
+
+
