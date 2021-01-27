e2623fc553a44729ecbf181530fea02a0cc019be
Tony Ngo <contact@suas.com>
2021-01-26 19:03:38 -0500

    show a modal when the followers or following is clicked

--- react-app/src/components/ProfilePage/index.js
+++ react-app/src/components/ProfilePage/index.js
@@ -11,29 +11,66 @@ const ProfilePage = () => {
   const dispatch = useDispatch();
   const profile = useSelector((state) => state.profile);
   const user = useSelector((state) => state.session.user);
-  const [numberOfFollowers, setNumberOfFollowers] = useState(0)
-  const [numberOfFollowing, setNumberOfFollowing] = useState(0)
-  const [numberOfOwnPosts, setNumberOfOwnPosts] = useState(0)
+  const [numberOfFollowers, setNumberOfFollowers] = useState(0);
+  const [numberOfFollowing, setNumberOfFollowing] = useState(0);
+  const [numberOfOwnPosts, setNumberOfOwnPosts] = useState(0);
+  const [showFollowersModal, setShowFollowersModal] = useState(false);
+  const [showFollowingModal, setShowFollowingModal] = useState(false);
 
   useEffect(() => {
     dispatch(fetchUserProfile(username));
   }, [dispatch, username]);
 
   useEffect(() => {
-    if(!profile.user) return;
-    if(profile.user.followers && Array.isArray(profile.user.followers)){
+    if (!profile.user) return;
+    if (profile.user.followers && Array.isArray(profile.user.followers)) {
       setNumberOfFollowers(profile.user.followers.length)
     }
-    if(profile.user.following && Array.isArray(profile.user.following)){
+    if (profile.user.following && Array.isArray(profile.user.following)) {
       setNumberOfFollowing(profile.user.following.length)
     }
-    if(profile.user.ownPosts && Array.isArray(profile.user.ownPosts)){
+    if (profile.user.ownPosts && Array.isArray(profile.user.ownPosts)) {
       setNumberOfOwnPosts(profile.user.ownPosts.length)
     }
   }, [profile]);
 
+  const handleFollowersClick = e => {
+    e.preventDefault();
+    setShowFollowersModal(true);
+  }
+
+  const handleFollowingClick = e => {
+    e.preventDefault();
+    setShowFollowingModal(true);
+  }
+
+  // window.addEventListener('click', e =>{
+  //   setShowFollowersModal(false);
+  //   setShowFollowingModal(false);
+  // });
+
+  const hideModal = e => {
+    e.preventDefault();
+    setShowFollowersModal(false);
+    setShowFollowingModal(false);
+  }
+
+  const FollowModal = ({ listOfUsers }) => {
+    return (
+      <div className='modal'
+        onClick={hideModal}
+      >
+        <div className='modal-content'>
+          <h1>Followers</h1>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="profile-page-container">
+      { showFollowersModal && <FollowModal /> }
+      { showFollowingModal && <FollowModal /> }
       {profile.user && (
         <div className="profile-info">
           <img
@@ -56,20 +93,34 @@ const ProfilePage = () => {
                 <span className="profile-number">{numberOfOwnPosts}</span>
                 <span className="profile-number-text"> posts</span>
               </div>
-              <Link
+              <div
+                className="profile-follower-numbers"
+                onClick={handleFollowersClick}
+              >
+                <span className="profile-number">{numberOfFollowers}</span>
+                <span className="profile-number-text"> followers</span>
+              </div>
+              <div
+                className="profile-follower-numbers"
+                onClick={handleFollowingClick}
+              >
+                <span className="profile-number">{numberOfFollowing}</span>
+                <span className="profile-number-text"> following</span>
+              </div>
+              {/* <Link
                 to={`/${username}/followers`}
                 className="profile-follower-numbers"
               >
                 <span className="profile-number">{numberOfFollowers}</span>
                 <span className="profile-number-text"> followers</span>
-              </Link>
-              <Link
+              </Link> */}
+              {/* <Link
                 to={`/${username}/following`}
                 className="profile-follower-numbers"
               >
                 <span className="profile-number">{numberOfFollowing}</span>
                 <span className="profile-number-text"> following</span>
-              </Link>
+              </Link> */}
             </div>
             <div className="profile-display-name">{profile.user.name}</div>
             <div className="profile-bio">{profile.user.bio}</div>
