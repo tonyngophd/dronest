8d1f395edd06cf51feaf1202b99c671dc909c574
Tony Ngo <contact@suas.com>
2021-01-28 10:14:45 -0500

    start messages page

---
+++ react-app/src/components/MessagePage/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+import './MessagePage.css'
+
+
+function MessagePage(){
+  return (
+    <div className='message-page-main-div'>
+      <div className='message-page-left-panel'>
+        Left
+      </div>
+      <div className='message-page-right-panel'>
+        Right
+      </div>
+    </div>
+  )
+}
+
+export default MessagePage;
