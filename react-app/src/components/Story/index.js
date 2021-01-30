c6b4b5b2811b7aeeb641bf32988e47357ea964ee
Tony Ngo <contact@suas.com>
2021-01-30 17:07:17 -0500

    first render the wholepage stories

--- react-app/src/components/Story/index.js
+++ react-app/src/components/Story/index.js
@@ -8,9 +8,8 @@ import './Story.css';
 import { nanoid } from 'nanoid';
 
 export function StoryTopBox() {
-  const [notifHover, setNotifHover] = useState(false);
   const [openStory, setOpenStory] = useState(false);
-
+  const [stories, updateStories] = useState([]);  
 
   // this is temporary
   const users = useSelector(state => state.users.allUsers);
@@ -22,56 +21,95 @@ export function StoryTopBox() {
       user => user.ownPosts.length
     ))
   }, [users])
+  useEffect(() => {
+    updateStories(
+      usersWithRecentPosts.map(user => {
+        return {
+          url: user.ownPosts[0].images[0].imgUrl,
+          duration: 2000,
+          header: {
+            heading: user.username,
+            subheading: 'Posted 30m ago',
+            profileImage: user.profilePicUrl,
+          },
+          // seeMore: ({ close }) => {
+          //   return <div onClick={close}>Hello, click to close this.</div>;
+          // },
+        }
+      })
+    )
+  }, [usersWithRecentPosts]);  
 
 
-  const stories = [
-    'https://example.com/pic.jpg',
-    {
-      url: 'https://example.com/pic2.jpg',
-      duration: 5000,
-      // seeMore: SeeMore, // some component
-    },
-    {
-      url: 'https://example.com/pic3.jpg',
-      duration: 2000,
-      seeMore: ({ close }) => {
-        return <div onClick={close}>Hello, click to close this.</div>;
-      },
-    },
-  ];
 
   return (
-    <div
-      className="feed_container story-topbox-container"
-    >
-      <div className='feed_post-container feed_post-info story-topbox-main-div'>
-        {
-          usersWithRecentPosts && usersWithRecentPosts.map(user =>
-            <div key={nanoid()}>
-              <Link to={`/${user.username}`}>
+    <>
+      {
+        openStory && stories.length && <StoriesFullPage stories={stories}/>
+      }
+      <div
+        className="feed_container story-topbox-container"
+      >
+        <div className='feed_post-container feed_post-info story-topbox-main-div'>
+          {
+            usersWithRecentPosts && usersWithRecentPosts.map(user =>
+              <div key={nanoid()}>
+                {/* <Link to={`/stories/${user.username}`}> */}
                 <div className="feed_post-header story-topbox-user-div">
                   <img src={user.profilePicUrl} alt="user-icon" className="story-profile-image"
                     style={{ width: '60px', height: '60px', borderRadius: '50%' }}
+                    onClick={e => setOpenStory(true)}
                   />
                   <div className="feed_post-username story-username-div">{user.username}</div>
                 </div>
-              </Link>
-            </div>)
-        }
+                {/* </Link> */}
+              </div>)
+          }
+        </div>
       </div>
-      {
-        openStory && <Stories
-          stories={stories}
-          width={300}
-          height={500}
-        />
-      }
-      {/* <div onClick={() => setIsPostOpen(true)} className="post-tab-new-post">
-      <BsPlusSquare className="hvr-grow" />
-    </div>
-    <div onClick={() => setIsPostOpen(true)} className="post-tab-new-story">
-      <BsClockHistory className="hvr-grow" />
-    </div> */}
-    </div>
+    </>
   )
+}
+
+export function StoriesFullPage({stories = []}) {
+  // const users = useSelector(state => state.users.allUsers);
+  // const [usersWithRecentPosts, setUsersWithRecentPosts] = useState([]);
+  // const [openStory, setOpenStory] = useState(false);
+  // const [stories, updateStories] = useState([]);
+
+  // useEffect(() => {
+  //   if (!users) return;
+  //   setUsersWithRecentPosts(users.filter(
+  //     user => user.ownPosts.length
+  //   ))
+  // }, [users])
+
+  // useEffect(() => {
+  //   updateStories(
+  //     usersWithRecentPosts.map(user => {
+  //       return {
+  //         url: user.ownPosts[0].images[0].imgUrl,
+  //         duration: 2000,
+  //         header: {
+  //           heading: user.username,
+  //           subheading: 'Posted 30m ago',
+  //           profileImage: user.profilePicUrl,
+  //         },
+  //         // seeMore: ({ close }) => {
+  //         //   return <div onClick={close}>Hello, click to close this.</div>;
+  //         // },
+  //       }
+  //     })
+  //   )
+  // }, [usersWithRecentPosts]);
+
+  return (
+    <div className='story-fullpage-container'>
+      <Stories
+        stories={stories}
+        width={600}
+        height={800}
+      />
+    </div>
+  );
 }
