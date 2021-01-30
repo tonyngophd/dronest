5a43baa35b380eda171dde1201406693a55aba6c
Daniel Miller <dbmiller25@gmail.com>
2021-01-30 02:24:37 -0500

    Clean up navbar and searchbar

--- react-app/src/components/Search/index.js
+++ react-app/src/components/Search/index.js
@@ -1,63 +1,82 @@
 import React from "react";
-import { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
-import Suggestions from '../Suggestions';
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import Suggestions from "../Suggestions";
 import { BareUserList } from "../UserLists";
+import { BsSearch, BsX } from "react-icons/bs";
+import * as searchActions from "../../store/search";
 
-import * as searchActions from '../../store/search';
-
-import './Search.css';
+import "./Search.css";
 
 export default function MainSearchBar({
-  className = 'search-over-banner-div'
+  className = "search-over-banner-div",
 }) {
   const [searchValue, setSearchValue] = useState("");
   const dispatch = useDispatch();
+  const [focused, setFocused] = useState(false);
 
   useEffect(() => {
     dispatch(searchActions.setSearchPOJO({ text: searchValue }));
   }, [searchValue]);
 
-  const onInputChange = e => {
+  const onInputChange = (e) => {
     e.preventDefault();
     setSearchValue(e.target.value);
     // console.log("search box value:", e.target.value);
-  }
+  };
 
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     dispatch(searchActions.setSearchPOJO({ text: searchValue }));
     setSearchValue("");
-  }
+  };
   return (
-    <div className="search-and-dropdown-display-main-box">
-      <form
-        type='submit'
-        className={className}
-        onSubmit={handleSubmit}
-      >
-        <i className="fas fa-search search-icon-class" ></i>
+    <div
+      className={
+        focused
+          ? "search-and-dropdown-display-main-box focused-search"
+          : "search-and-dropdown-display-main-box"
+      }
+    >
+      <form type="submit" className={className} onSubmit={handleSubmit}>
+        <BsSearch className="search-icon" />
         <input
-          className='main-search-bar'
-          type='text'
-          placeholder='Search...'
+          className="main-search-bar"
+          type="text"
+          placeholder="Search..."
+          onBlur={(e) => {
+            e.preventDefault();
+            setFocused(false);
+            setSearchValue("");
+          }}
           value={searchValue}
           onChange={onInputChange}
-          autoFocus={window.location.pathname.includes('allspots') ? true : false}
-        >
-        </input>
-        <i className="fas fa-times search-icon-class"
-          style={{ color: 'lightgray' }}
-          onClick={e => { e.preventDefault(); setSearchValue("") }}
+          onFocus={() => setFocused(true)}
+          autoFocus={
+            window.location.pathname.includes("allspots") ? true : false
+          }
+        ></input>
+        <BsX
+          className="search-icon-x"
+          onClick={(e) => {
+            e.preventDefault();
+            setSearchValue("");
+          }}
         />
       </form>
-      <div className='search-dropdown'>
-        {
-          searchValue && <Suggestions UserList={BareUserList} 
-            searchable={true} searchText={searchValue} 
-            style={{maxHeight: "300px", overflowY: "scroll", marginLeft:'-160px'}}
-            />
-        }
+      <div className="search-dropdown">
+        {searchValue && (
+          <Suggestions
+            UserList={BareUserList}
+            searchable={true}
+            searchText={searchValue}
+            style={{
+              maxHeight: "300px",
+              overflowY: "scroll",
+              marginLeft: "-160px",
+            }}
+          />
+        )}
       </div>
     </div>
   );
