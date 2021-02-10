e798d7c5097e35285fd9d4ab78429a150767229c
Tony Ngo <contact@suas.com>
2021-02-10 15:46:26 -0500

    highlight the inputs that have issues as red-bordered box

--- react-app/src/components/auth/ChangePasswordForm.js
+++ react-app/src/components/auth/ChangePasswordForm.js
@@ -12,19 +12,22 @@ const ChangePasswordForm = ({ setShowModal }) => {
   const [errors, setErrors] = useState([]);
   const [messages, setMessages] = useState([]);
   const [password, setPassword] = useState("");
+  const [passwordIsSubmitting, setPasswordIsSubmitting] = useState(false);
   const [newPassword, setNewPassword] = useState("");
+  const [newPasswordIsSubmitting, setNewPasswordIsSubmitting] = useState(false);  
   const [newConfirmPassword, setNewConfirmPassword] = useState("");
+  const [newCPasswordIsSubmitting, setNewCPasswordIsSubmitting] = useState(false);  
 
   const onChangePassword = async (e) => {
     e.preventDefault();
     if (newPassword !== newConfirmPassword || !password || !newPassword) {
       const errs = [];
       if (!password)
-        errs.push(["Please provide current password"]);
+        errs.push("Please provide current password");
       if (!newPassword)
-        errs.push(["Please provide new password"]);
+        errs.push("Please provide new password");
       if (newPassword !== newConfirmPassword)
-        errs.push(["Passwords do not match"]);
+        errs.push("Passwords do not match");
 
       setErrors(errs);
     }
@@ -40,9 +43,11 @@ const ChangePasswordForm = ({ setShowModal }) => {
     }
   };
 
-  const updatePassword = (e, cb = setPassword) => {
+  const updatePassword = (e, cb = setPassword, cb2 = setPasswordIsSubmitting) => {
     cb(e.target.value);
+    cb2(false);
   };
+  
 
   if (!user) {
     return <Redirect to="/login" />;
@@ -61,6 +66,13 @@ const ChangePasswordForm = ({ setShowModal }) => {
   const escapeHideModal = e => {
     if (e.key === 'Escape')
       setShowModal(false);
+  }
+
+  const onSubmitClick = e => {
+    e.stopPropagation();
+    setPasswordIsSubmitting(true);
+    setNewPasswordIsSubmitting(true);
+    setNewCPasswordIsSubmitting(true);    
   }
 
   return (
@@ -85,7 +97,10 @@ const ChangePasswordForm = ({ setShowModal }) => {
             ))}
           </div> :
             <>
-              <div className="login-form-element">
+              <div className={passwordIsSubmitting && 
+                  errors.includes("Please provide current password")? 
+                  "login-form-element-error" 
+                  :"login-form-element"}>
                 <label htmlFor="password"></label>
                 <input
                   name="password"
@@ -96,28 +111,34 @@ const ChangePasswordForm = ({ setShowModal }) => {
                   autoFocus={true}
                 />
               </div>
-              <div className="login-form-element">
+              <div className={newPasswordIsSubmitting && 
+                  errors.includes("Please provide new password")? 
+                  "login-form-element-error" 
+                  :"login-form-element"}>
                 <label htmlFor="password"></label>
                 <input
                   name="password"
                   type="password"
                   placeholder="New Password"
                   value={newPassword}
-                  onChange={e => updatePassword(e, setNewPassword)}
+                  onChange={e => updatePassword(e, setNewPassword, setNewPasswordIsSubmitting)}
                 />
               </div>
-              <div className="login-form-element">
+              <div className={newCPasswordIsSubmitting && 
+                  errors.includes("Passwords do not match")? 
+                  "login-form-element-error" 
+                  :"login-form-element"}>
                 <label htmlFor="password"></label>
                 <input
                   name="password"
                   type="password"
                   placeholder="Confirm New Password"
                   value={newConfirmPassword}
-                  onChange={e => updatePassword(e, setNewConfirmPassword)}
+                  onChange={e => updatePassword(e, setNewConfirmPassword, setNewCPasswordIsSubmitting)}
                 />
               </div>
               <div className="buttons">
-                <button type="submit" id="login-button" onClick={e => e.stopPropagation()}>Change Password</button>
+                <button type="submit" id="login-button" onClick={onSubmitClick}>Change Password</button>
                 <button id="cancel-button" className='cancel-button' onClick={e => setShowModal(false)}>Cancel</button>
               </div>
             </>}
