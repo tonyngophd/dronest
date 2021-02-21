007b112f4ec05a86de8b25396d8fc13a4feb473f
Tony Ngo <contact@suas.com>
2021-02-21 16:49:03 -0500

    add drone images

--- app/models/equipment.py
+++ app/models/equipment.py
@@ -6,6 +6,7 @@ class Equipment(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(50), nullable=False, unique=True)
   level = db.Column(db.Integer, nullable=False)
+  mediaUrl = db.Column(db.Text, nullable=False)
 
   createdAt = db.Column(db.DateTime(timezone=True), server_default=db.func.now()) #func.sysdate())
   updatedAt = db.Column(db.DateTime(timezone=True), server_default=db.func.now(), server_onupdate=db.func.now())
