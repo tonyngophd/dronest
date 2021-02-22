71e36db684f55643df4a5a9bcacb7afad8cd37c1
Tony Ngo <contact@suas.com>
2021-02-21 20:51:52 -0500

    style poping out better

--- react-app/src/components/ProfilePage/Equipment.js
+++ react-app/src/components/ProfilePage/Equipment.js
@@ -11,12 +11,14 @@ export function EquipmentCard({ equipment }) {
 
   return (
     <div className='equipment_card_div'>
+      <div className='equipment_card_innver_div'>
       <img src={equipment.mediaUrl}
         alt={equipment.mediaUrl}
         className='equipment-img'
       />
       <div className='equipment_name_div'>
         {equipment.name}
+      </div>
       </div>
     </div>
   );
@@ -27,12 +29,15 @@ export function EquipmentDiv({ equipmentList }) {
   return (
     <div className='equipment_list_container'>
       {
-        equipmentList.map(eq =>
-          <EquipmentCard
-            equipment={eq}
-            key={nanoid()}
-          />
-        )}
+        equipmentList
+          .sort((a, b) =>
+            a.level < b.level ? 1 : -1)
+          .map(eq =>
+            <EquipmentCard
+              equipment={eq}
+              key={nanoid()}
+            />
+          )}
     </div>
   );
 }
