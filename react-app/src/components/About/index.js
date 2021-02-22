25d97c2fe8ff5fd4b28399117c6fa387291637cb
Tony Ngo <contact@suas.com>
2021-02-22 16:00:41 -0500

    style the about page

--- react-app/src/components/About/index.js
+++ react-app/src/components/About/index.js
@@ -1,16 +1,35 @@
 import React, { useEffect, useState } from 'react';
-import Iframe from 'react-iframe'
-import ReactMarkdown from 'react-markdown'
-import { render } from 'react-dom'
+import { useSelector, useDispatch } from 'react-redux';
+// import Iframe from 'react-iframe';
+import ReactMarkdown from 'react-markdown';
+import { render } from 'react-dom';
+import MiniProfile from '../MiniProfile';
+
+import { fetchAllUsers } from '../../store/users';
 
 
 import './About.css';
 
 
 export function About() {
+  const users = useSelector(state => state.users.allUsers);
   const url = 'https://api.github.com/repos/suasllc/dronest/contents/README.md';
-
   let [gotContents, setGetContents] = useState(false)
+  const dispatch = useDispatch();
+  const [author, setAuthor] = useState(null);
+
+
+  useEffect(() => {
+    if (!users.length) {
+      dispatch(fetchAllUsers());
+    }
+    if (users.length >= 3) setAuthor(users[2]);
+  }, [dispatch]);
+  useEffect(() => {
+    if (!author && users.length >= 3) setAuthor(users[2]);
+  }, [users]);
+
+
   const fetchGit = async (url) => {
     const res = await fetch(url);
     if (res.ok) {
@@ -23,49 +42,51 @@ export function About() {
 
   useEffect(() => {
     fetchGit(url);
-    // if(iframe)
-    //   console.log('iframe', iframe.innerHTML);    
   }, [url]);
 
-  useEffect(() => {
-    // console.log(iframe, gotContents);
-  }, [gotContents, url]);
 
   return (
     <div className='about_page_container'>
-      <img src={'https://image.flaticon.com/icons/png/512/25/25231.png'} className='technology-icon' />
-      <div>
+      <div className='technology-and-author'>
         <div>
-          Package
+          <div>
+            <div>
+              Package
+          </div>
+            <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Npm-logo.svg/1280px-Npm-logo.svg.png' className='technology-icon' />
+            <img src='https://www.docker.com/sites/default/files/d8/2019-07/vertical-logo-monochromatic.png' className='technology-icon' />
+            <img src='https://cdn.iconscout.com/icon/free/png-512/postgresql-11-1175122.png' className='technology-icon' />
+            <img src='https://cdn.worldvectorlogo.com/logos/heroku.svg' className='technology-icon' />
+            <div>
+              Frontend
+          </div>
+            <img src={require('../../pictures/javascript.svg')} className='technology-icon' />
+            <img src={require('../../pictures/iconfinder_React.js_logo_1174949.svg')} className='technology-icon' />
+            <img src={require('../../pictures/iconfinder_redux_4691205.svg')} className='technology-icon' />
+            <img src={require('../../pictures/css.svg')} className='technology-icon' />
+            <img src={require('../../pictures/html5.png')} className='technology-icon' />
+          </div>
+          <div>
+            <div>
+              Backend
+          </div>
+            <div>
+              Main Server
+          </div>
+            <img src={require('../../pictures/python.png')} className='technology-icon' />
+            <img src={'https://miro.medium.com/max/800/1*Q5EUk28Xc3iCDoMSkrd1_w.png'} className='technology-icon' />
+            <img src={'https://flask-sqlalchemy.palletsprojects.com/en/2.x/_static/flask-sqlalchemy-logo.png'} className='technology-icon' />
+            <div>
+              Instant Messaging Server
+          </div>
+            <img src={require('../../pictures/nodejs.svg')} className='technology-icon' />
+            <img src={'https://www.logolynx.com/images/logolynx/5b/5bf98b408fb57fec23637f44edd79138.jpeg'} className='technology-icon' />
+          </div>
         </div>
-        <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Npm-logo.svg/1280px-Npm-logo.svg.png' className='technology-icon' />
-        <img src='https://www.docker.com/sites/default/files/d8/2019-07/vertical-logo-monochromatic.png' className='technology-icon' />
-        <img src='https://cdn.iconscout.com/icon/free/png-512/postgresql-11-1175122.png' className='technology-icon' />
-        <img src='https://cdn.worldvectorlogo.com/logos/heroku.svg' className='technology-icon' />
         <div>
-          Frontend
+          {author && <MiniProfile user={author} hover={true} className='miniprofile-container-div static' />}
+          <img src={'https://image.flaticon.com/icons/png/512/25/25231.png'} className='technology-icon' />
         </div>
-        <img src={require('../../pictures/javascript.svg')} className='technology-icon' />
-        <img src={require('../../pictures/iconfinder_React.js_logo_1174949.svg')} className='technology-icon' />
-        <img src={require('../../pictures/iconfinder_redux_4691205.svg')} className='technology-icon' />
-        <img src={require('../../pictures/css.svg')} className='technology-icon' />
-        <img src={require('../../pictures/html5.png')} className='technology-icon' />
-      </div>
-      <div>
-        <div>
-          Backend
-        </div>
-        <div>
-          Main Server
-        </div>
-        <img src={require('../../pictures/python.png')} className='technology-icon' />
-        <img src={'https://miro.medium.com/max/800/1*Q5EUk28Xc3iCDoMSkrd1_w.png'} className='technology-icon' />
-        <img src={'https://flask-sqlalchemy.palletsprojects.com/en/2.x/_static/flask-sqlalchemy-logo.png'} className='technology-icon' />
-        <div>
-          Instant Messaging Server
-        </div>
-        <img src={require('../../pictures/nodejs.svg')} className='technology-icon' />
-        <img src={'https://www.logolynx.com/images/logolynx/5b/5bf98b408fb57fec23637f44edd79138.jpeg'} className='technology-icon' />
       </div>
       <div id='markdown'></div>
     </div>
